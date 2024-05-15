4758a1782ca92f40141245c6af37592ad732e5b5
MoonShiesty <moonshiesty@protonmail.com>
2024-05-15 00:43:13 +0000

    remove mirage.rs

--- rust/processor/src/models/mirage.rs
+++
@@ -1,5 +0,0 @@
-// Copyright © Mirage Protocol
-
-pub const MIRAGE_ADDR: &str = "0x20f2ef7f31cab15a3c0bf012a2ba6d9fba0fc131993fd999cf82a065c3e8326e";
-pub const MIRAGE_CORE_ADDR: &str =
-    "0xf9925f48fb67e62e17914f79755177dce10dcf8f14ea392ae9c9bb40a62a8cb3";
